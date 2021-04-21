161bbf02f2b17423a2a662b9081559f7d536130a
bhanu <you@example.com>
2021-04-21 19:54:37 +0200

    addded env variables

--- config/database.js
+++ config/database.js
@@ -1,4 +1,9 @@
 // config/database.js
+const dotenv = require("dotenv");
+dotenv.config();
 module.exports = {
-  url: "mongodb://localhost/weather",
+  PORT: process.env.PORT,
+  url: process.env.DB_URL,
+  api_key: process.env.API_KEY,
+  api_url: process.env.API_URL,
 };
